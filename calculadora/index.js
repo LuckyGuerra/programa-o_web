20172a6b51d85b9fcdd90f994dd1b6c83018825f
LuckyGuerra <125315615+LuckyGuerra@users.noreply.github.com>
2025-03-25 19:22:25 -0300

    Update index.js

--- calculadora/index.js
+++ calculadora/index.js
@@ -1,6 +1,29 @@
-const calc = require('./calculadora')
+const express = require('express');
+const calculadora = require('./calculadora');
 
-console.log(calc.somar(2, 3));
-console.log(calc.subtrair(2, 3));
-console.log(calc.multiplicar(2, 3));
-console.log(calc.dividir(2, 3));
+const app = express();
+
+app.get('/soma/:x/:y', (req, res)=>{
+    const { x, y } = req.params;
+    res.send(`sua conta é: ${calculadora.somar(Number(x), Number(y))}`);
+});
+
+app.get('/subtracao/:x/:y', (req, res)=>{
+    const { x, y } = req.params;
+    res.send(`sua conta é: ${calculadora.subtrair(Number(x), Number(y))}`);
+});
+
+app.get('/multiplicacao/:x/:y', (req, res)=>{
+    const { x, y } = req.params;
+    res.send(`sua conta é: ${calculadora.multiplicar(Number(x), Number(y))}`);
+});
+
+app.get('/divisao/:x/:y', (req, res)=>{
+    const { x, y } = req.params;
+    res.send(`sua conta é: ${calculadora.dividir(Number(x), Number(y))}`);
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`app rodando na porta ${PORT}`);
+});
