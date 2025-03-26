5a73416c5e67bad289ceb8aabcecea9e8fd713bb
LuckyGuerra <125315615+LuckyGuerra@users.noreply.github.com>
2025-03-25 21:06:04 -0300

    Update index.js

--- calculadora/index.js
+++ calculadora/index.js
@@ -3,27 +3,27 @@ const calculadora = require('./calculadora');
 
 const app = express();
 
-app.get('/soma/:x/:y', (req, res)=>{
+app.get('/soma/:x/:y', (req, res) => {
     const { x, y } = req.params;
-    res.send(`sua conta é: ${calculadora.somar(Number(x), Number(y))}`);
+    res.send(Sua conta é: ${calculadora.somar(Number(x), Number(y))});
 });
 
-app.get('/subtracao/:x/:y', (req, res)=>{
+app.get('/subtracao/:x/:y', (req, res) => {
     const { x, y } = req.params;
-    res.send(`sua conta é: ${calculadora.subtrair(Number(x), Number(y))}`);
+    res.send(Sua conta é: ${calculadora.subtrair(Number(x), Number(y))});
 });
 
-app.get('/multiplicacao/:x/:y', (req, res)=>{
+app.get('/multiplicacao/:x/:y', (req, res) => {
     const { x, y } = req.params;
-    res.send(`sua conta é: ${calculadora.multiplicar(Number(x), Number(y))}`);
+    res.send(Sua conta é: ${calculadora.multiplicar(Number(x), Number(y))});
 });
 
-app.get('/divisao/:x/:y', (req, res)=>{
+app.get('/divisao/:x/:y', (req, res) => {
     const { x, y } = req.params;
-    res.send(`sua conta é: ${calculadora.dividir(Number(x), Number(y))}`);
+    res.send(Sua conta é: ${calculadora.dividir(Number(x), Number(y))});
 });
 
 const PORT = 8080;
 app.listen(PORT, () => {
-    console.log(`app rodando na porta ${PORT}`);
+    console.log(App rodando na porta ${PORT});
 });
